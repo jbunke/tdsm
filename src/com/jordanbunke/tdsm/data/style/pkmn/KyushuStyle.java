bfa55fe44f1dc1e0664ed03d39e2841170e050a5
Jordan Bunke <jordantb99@gmail.com>
2025-03-14 21:47:21 +0000

    [Kyushu] + Added head movement

--- src/com/jordanbunke/tdsm/data/style/pkmn/KyushuStyle.java
+++ src/com/jordanbunke/tdsm/data/style/pkmn/KyushuStyle.java
@@ -1,17 +1,26 @@
 package com.jordanbunke.tdsm.data.style.pkmn;
 
+import com.jordanbunke.delta_time.image.GameImage;
+import com.jordanbunke.delta_time.sprite.SpriteSheet;
+import com.jordanbunke.delta_time.sprite.SpriteStates;
+import com.jordanbunke.delta_time.sprite.constituents.InterpretedSpriteSheet;
+import com.jordanbunke.delta_time.sprite.constituents.SpriteConstituent;
 import com.jordanbunke.delta_time.utility.math.Bounds2D;
 import com.jordanbunke.delta_time.utility.math.Coord2D;
 import com.jordanbunke.tdsm.data.Animation;
 import com.jordanbunke.tdsm.data.Animation.PlaybackMode;
+import com.jordanbunke.tdsm.data.Directions;
+import com.jordanbunke.tdsm.data.Directions.Dir;
 import com.jordanbunke.tdsm.data.func.ColorReplacementFunc;
 import com.jordanbunke.tdsm.data.layer.AssetChoiceLayer;
 import com.jordanbunke.tdsm.data.layer.ColorSelectionLayer;
+import com.jordanbunke.tdsm.data.layer.PureComposeLayer;
 import com.jordanbunke.tdsm.data.layer.builders.ACLBuilder;
 import com.jordanbunke.tdsm.data.layer.support.AssetChoiceTemplate;
 import com.jordanbunke.tdsm.data.layer.support.ColorSelection;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 public final class KyushuStyle extends PokemonStyle {
     private static final KyushuStyle INSTANCE;
@@ -92,9 +101,21 @@ public final class KyushuStyle extends PokemonStyle {
 
         layers.addToCustomization(skinLayer, bodyLayer, headLayer);
 
-        // TODO
+        final PureComposeLayer combinedHeadLayer =
+                new PureComposeLayer("combined-head", spriteID -> {
+                    final GameImage preassembled = new GameImage(HEAD_SHEET_DIMS);
 
-        layers.addToAssembly(bodyLayer);
+                    preassembled.draw(headLayer.compose().getSprite(spriteID));
+                    // TODO
+
+                    final SpriteSheet combined =
+                            new SpriteSheet(preassembled.submit(),
+                                    HEAD_DIMS.width(), HEAD_DIMS.height());
+
+                    return composeHead(combined).getSprite(spriteID);
+                });
+
+        layers.addToAssembly(bodyLayer, combinedHeadLayer);
     }
 
     private AssetChoiceTemplate[] buildTemplates(final String... ids) {
@@ -107,5 +128,110 @@ public final class KyushuStyle extends PokemonStyle {
         return Arrays.stream(ids)
                 .map(id -> new AssetChoiceTemplate(id, f))
                 .toArray(AssetChoiceTemplate[]::new);
+    }
+
+    private SpriteConstituent<String> composeHead(final SpriteSheet sheet) {
+        final SpriteConstituent<String> assetFetcher =
+                new InterpretedSpriteSheet<>(sheet, this::headDirX);
+
+        return id -> {
+            final GameImage sprite = new GameImage(DIMS.width(), DIMS.height());
+
+            final Directions.Dir dir = Directions.get(
+                    SpriteStates.extractContributor(DIRECTION, id));
+            final String animID =
+                    SpriteStates.extractContributor(ANIM, id);
+
+            if (animID.equals(ANIM_ID_CAPSULE) && !dir.equals(Directions.Dir.DOWN))
+                return sprite;
+
+            final Coord2D offset = headOffset(id);
+            final int BASE_X = 10, BASE_Y = 10,
+                    x = BASE_X + offset.x, y = BASE_Y + offset.y;
+
+            sprite.draw(assetFetcher.getSprite(id), x, y);
+            return sprite.submit();
+        };
+    }
+
+    private Coord2D headOffset(final String spriteID) {
+        final Directions.Dir dir = Directions.get(
+                SpriteStates.extractContributor(DIRECTION, spriteID));
+        final String animID =
+                SpriteStates.extractContributor(ANIM, spriteID);
+        final int frame = Integer.parseInt(
+                SpriteStates.extractContributor(FRAME, spriteID));
+
+        final int animComp, frameComp, x;
+
+        // frame functions
+        final Function<Integer, Integer> rock3 = f -> f % 2 == 0 ? f - 1 : 0,
+                fishSide = f -> switch (f) {
+            case 0 -> 3;
+            case 2 -> 1;
+            default -> 2;
+        };
+
+        x = switch (animID) {
+            case ANIM_ID_CYCLE -> rock3.apply(frame);
+            case ANIM_ID_FISH -> switch (dir) {
+                case DOWN -> frame == 3 ? 0 : 1;
+                case UP -> frame > 1 ? 0 : -1;
+                case RIGHT -> fishSide.apply(frame) * -1;
+                default -> fishSide.apply(frame);
+            };
+            case ANIM_ID_CAPSULE -> switch (frame) {
+                case 1, 4 -> -1;
+                default -> 0;
+            };
+            case ANIM_ID_SURF -> switch (dir) {
+                case LEFT -> 1;
+                case RIGHT -> -1;
+                default -> 0;
+            };
+            default -> 0;
+        };
+
+        // animation Y offset component
+        animComp = switch (animID) {
+            case ANIM_ID_IDLE, ANIM_ID_WALK -> switch (dir) {
+                case DOWN -> 2;
+                case UP -> 4;
+                default -> 3;
+            };
+            case ANIM_ID_RUN -> 2 + (dir.equals(Dir.DOWN) ? 0 : 1);
+            case ANIM_ID_CYCLE -> 1 + (dir.equals(Dir.UP) ? 1 : 0);
+            case ANIM_ID_SURF -> -4 - switch (dir) {
+                case DOWN -> 2;
+                case UP -> 0;
+                default -> 1;
+            };
+            // TODO - capsule, fish
+            default -> 0;
+        };
+
+        // animation frame Y offset component
+        frameComp = switch (animID) {
+            case ANIM_ID_WALK, ANIM_ID_RUN -> frame != 1 ? 1 : 0;
+            case ANIM_ID_SURF -> frame;
+            // TODO - capsule, fish
+            default -> 0;
+        };
+
+        return new Coord2D(x, animComp + frameComp);
+    }
+
+    private Coord2D headDirX(final String spriteID) {
+        final Directions.Dir dir = Directions.get(
+                SpriteStates.extractContributor(DIRECTION, spriteID));
+        final String animID =
+                SpriteStates.extractContributor(ANIM, spriteID);
+
+        final int x = indexOfDir(dir);
+
+        final boolean tiltedDown = dir.equals(Directions.Dir.DOWN) &&
+                animID.equals(ANIM_ID_RUN);
+
+        return new Coord2D(x + (tiltedDown ? 4 : 0), 0);
     }
 }
