b85363e8d982a14cf427533f7567609ac92c9d32
Jordan Bunke <jordantb99@gmail.com>
2026-01-03 16:43:38 -0700

    > Updated program icon > Updated Windows build script to create separate installer and self-contained app artifacts

--- config/win_build.bat
+++ config/win_build.bat
@@ -2,18 +2,22 @@
 
 setlocal
 
+set "ID=tdsm"
 set "NAME=Top Down Sprite Maker"
+set "DEV=Jordan Bunke"
 
 echo Building %NAME% for Windows...
 
 set "ARTIFACT_DIR=..\out\artifacts\release"
 set "OUTPUT_DIR=..\out\artifacts\dist\win"
 
+rd /s /q "%OUTPUT_DIR%\build"
+rd /s /q "%OUTPUT_DIR%\runtime"
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 
 :: Analyze Java module dependencies
 jdeps --multi-release 17 --print-module-deps "%ARTIFACT_DIR%\tdsm.jar" > "%OUTPUT_DIR%\modules.txt"
-echo 1/3: Analyzed dependencies
+echo 1/4: Analyzed dependencies
 
 :: Read modules from file
 set /p MODULES=<"%OUTPUT_DIR%\modules.txt"
@@ -23,7 +27,7 @@ echo Modules: %MODULES%
 set JAVA_MOD_PATH="C:\Program Files\Java\jdk-17\jmods"
 
 jlink --module-path %JAVA_MOD_PATH% --add-modules "%MODULES%" --output "%OUTPUT_DIR%\runtime"
-echo 2/3: Generated runtime image
+echo 2/4: Generated runtime image
 
 :: Build the EXE with Launch4J
 :: call l4j.bat
@@ -41,17 +45,34 @@ set ICON_PATH=".\icons\win-icon.ico"
 jpackage ^
     --type exe ^
     --input "%ARTIFACT_DIR%" ^
-    --dest "%OUTPUT_DIR%\build" ^
+    --dest "%OUTPUT_DIR%\installer" ^
     --name "%NAME%" ^
     --app-version "%VERSION%" ^
     --main-jar tdsm.jar ^
     --runtime-image "%OUTPUT_DIR%\runtime" ^
     --icon "%ICON_PATH%" ^
+    --vendor "%DEV%" ^
     --win-dir-chooser ^
     --win-shortcut ^
     --win-menu ^
     --win-menu-group "Top Down Sprite Maker" ^
     --description "%NAME%"
-echo 3/3: Built Windows executable with jpackage
+move "%OUTPUT_DIR%\installer\%NAME%-%VERSION%.exe" "%OUTPUT_DIR%\installer\%ID%-%VERSION%.exe"
+
+echo 3/4: Built Windows installer with jpackage
+
+jpackage ^
+    --type app-image ^
+    --input "%ARTIFACT_DIR%" ^
+    --dest "%OUTPUT_DIR%\build" ^
+    --name "%ID%" ^
+    --app-version "%VERSION%" ^
+    --main-jar tdsm.jar ^
+    --runtime-image "%OUTPUT_DIR%\runtime" ^
+    --icon "%ICON_PATH%" ^
+    --vendor "%DEV%" ^
+    --description "%NAME%"
+
+echo 4/4: Built Windows executable with jpackage
 
 endlocal
